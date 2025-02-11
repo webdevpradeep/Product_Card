e200c0df7e34af84a04d6ae012698f517597bf0d
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-02-11 20:55:53 +0530

    Product_Card

---
+++ main.js
@@ -0,0 +1,55 @@
+var nextBtn = document.querySelector('.next');
+prevBtn = document.querySelector('.prev');
+carousel = document.querySelector('.carousel');
+list = document.querySelector('.list');
+item = document.querySelectorAll('.item');
+runningTime = document.querySelector('.timeRunning');
+
+let timeRunning = 3000;
+let timeAutoNext = 7000;
+
+nextBtn.onclick = function () {
+  showSlider('next');
+};
+
+prevBtn.onclick = function () {
+  showSlider('prev');
+};
+
+let runTimeOut;
+let runNextAuto = setTimeout(() => {
+  nextBtn.click();
+}, timeAutoNext);
+
+function resetTimeAnimation() {
+  runningTime.style.animation = 'none';
+  runningTime.offsetHeight;
+  runningTime.style.aniamtion = null;
+  runningTime.style.animation = 'runningTime 7s linear 1 forwards';
+}
+
+function showSlider(type) {
+  let sliderItemDom = list.querySelectorAll('.carousel .list .item');
+  if (type === 'next') {
+    list.appendChild(sliderItemDom[0]);
+    carousel.classList.add('next');
+  } else {
+    list.prepend(sliderItemDom[sliderItemDom.length - 1]);
+    carousel.classList.add('perv');
+  }
+
+  clearTimeout(runTimeOut);
+
+  runTimeOut = setTimeout(() => {
+    carousel.classList.remove('next');
+    carousel.classList.remove('prev');
+  }, timeAutoNext);
+
+  clearTimeout(runNextAuto);
+  runNextAuto = setTimeout(() => {
+    nextBtn.click();
+  }, timeAutoNext);
+  resetTimeAnimation();
+}
+
+resetTimeAnimation();
