4eb89a1add6f981d9cd61e0f74421f35aa6431b8
Pradeep Maurya <pradeep25072004maurya@gmail.com>
2025-02-12 19:44:45 +0530

    Product_CardChange title

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var nextBtn = document.querySelector('.next');
-prevBtn = document.querySelector('.prev');
-carousel = document.querySelector('.carousel');
-list = document.querySelector('.list');
-item = document.querySelectorAll('.item');
-runningTime = document.querySelector('.timeRunning');
+var nextBtn = document.querySelector('.next'),
+  prevBtn = document.querySelector('.prev'),
+  carousel = document.querySelector('.carousel'),
+  list = document.querySelector('.list'),
+  item = document.querySelectorAll('.item'),
+  runningTime = document.querySelector('.carousel .timeRunning');
 
 let timeRunning = 3000;
 let timeAutoNext = 7000;
@@ -17,25 +17,26 @@ prevBtn.onclick = function () {
 };
 
 let runTimeOut;
+
 let runNextAuto = setTimeout(() => {
   nextBtn.click();
 }, timeAutoNext);
 
 function resetTimeAnimation() {
   runningTime.style.animation = 'none';
-  runningTime.offsetHeight;
-  runningTime.style.aniamtion = null;
+  runningTime.offsetHeight; /* trigger reflow */
+  runningTime.style.animation = null;
   runningTime.style.animation = 'runningTime 7s linear 1 forwards';
 }
 
 function showSlider(type) {
-  let sliderItemDom = list.querySelectorAll('.carousel .list .item');
+  let sliderItemsDom = list.querySelectorAll('.carousel .list .item');
   if (type === 'next') {
-    list.appendChild(sliderItemDom[0]);
+    list.appendChild(sliderItemsDom[0]);
     carousel.classList.add('next');
   } else {
-    list.prepend(sliderItemDom[sliderItemDom.length - 1]);
-    carousel.classList.add('perv');
+    list.prepend(sliderItemsDom[sliderItemsDom.length - 1]);
+    carousel.classList.add('prev');
   }
 
   clearTimeout(runTimeOut);
@@ -43,13 +44,15 @@ function showSlider(type) {
   runTimeOut = setTimeout(() => {
     carousel.classList.remove('next');
     carousel.classList.remove('prev');
-  }, timeAutoNext);
+  }, timeRunning);
 
   clearTimeout(runNextAuto);
   runNextAuto = setTimeout(() => {
     nextBtn.click();
   }, timeAutoNext);
-  resetTimeAnimation();
+
+  resetTimeAnimation(); // Reset the running time animation
 }
 
+// Start the initial animation
 resetTimeAnimation();
